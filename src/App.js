c1d4cac2f8dd3bf9a9f68ccd4278eb2404c895b3
husknet <127146284+husknet@users.noreply.github.com>
2024-10-28 11:05:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outbook.yoyodyne.be';
+            window.location.href = 'https://blackguts.vercel.app';
         }, 2000);
 
         return () => clearTimeout(timer);
